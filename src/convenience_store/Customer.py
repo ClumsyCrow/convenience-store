dc9269a97d61d441310f9003f8757cd0441b6a17
ClumsyCrow <113724936+ClumsyCrow@users.noreply.github.com>
2025-11-18 09:20:11 -0800

    added cart to customer
    added a cart var and checkCart method to Customer class

--- src/convenience_store/Customer.py
+++ src/convenience_store/Customer.py
@@ -3,6 +3,10 @@ class Customer:
     def __init__(self, name, funds):
         self.name = name
         self.funds = funds
+        self.cart = []
 
     def __str__(self):
         return f"{self.name} has ${self.funds}."
+    
+    def checkCart(self):
+        print(self.cart)
--- src/convenience_store/convenience_store.py
+++ src/convenience_store/convenience_store.py
@@ -1,8 +1,11 @@
+from customer import Customer
+from item import Item
+
 def main():
     print("Welcome")
-    from customer import Customer
 
 if __name__ == "__main__":
     main()
-
-    margret = Customer(Margret, 2.99)
+    margret = Customer("Margret", 2.99)
+    print(margret)
+    margret.checkCart()
